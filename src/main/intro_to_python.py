6d2f600082c0049a151246afb8e7d2baff301a1d
Hanmos3109 <122555522+Hanmos3109@users.noreply.github.com>
2023-01-19 21:58:58 -0500

    Create intro_to_python.py

---
+++ src/main/intro_to_python.py
@@ -0,0 +1,29 @@
+
+def printMatrix(matrix):
+    for a in Newarray:
+        print(" ".join(str(b)for b in a))
+    print()
+
+
+Newarray=[[0]*3 for _ in range(3)]
+
+
+#Identity matrix
+for i in range(len(Newarray)):
+    Newarray[i][i] = 1
+printMatrix(Newarray)
+
+#Replace 0 with 3
+for i in range(len(Newarray)):
+    for j in range(len(Newarray[i])):
+        if i == j:
+            Newarray[i][j] = 1
+        else:
+            Newarray[i][j] = 3
+    #Newarray[i][i] = 1
+printMatrix(Newarray)
+
+for a in Newarray:
+    a.pop()
+printMatrix(Newarray)
+
